5f2532dcd4494f4df56371cf7317827e36b05925
Jose Fresco <josemariafresco@gmail.com>
2014-03-14 17:51:41 +0000

    Added few tests

--- match.js
+++ match.js
@@ -13,21 +13,63 @@ var Match = (function () {
         player2Score = 0;
     }
 
+    var translateScore = function(score) {
+        switch (score) {
+            case 0:
+                return 'Love';
+            case 1:
+                return 'Fifteen';
+            case 2:
+                return 'Thirty';
+            case 3:
+                return 'Forty';
+            case 4:
+                return 'Advantage';
+            default:
+                throw new Error('Illegal score');    
+        }
+    };
+
     Match.prototype.player1Score =
     function() {
-        return player1Score;     
+        return translateScore(player1Score);     
     };
 
     Match.prototype.player2Score =
     function() {
-        return player2Score;
+        return translateScore(player2Score);
     };
 
     Match.prototype.player1Scores =
     function() {
         switch (player1Score) {
             case 0:
-                player1Score = 15;
+            case 1:
+            case 2:
+                player1Score++;
+                break;
+            case 3:
+               if (player2Score == 3) {
+                   player1Score++;
+               } else if (player2Score == 4) {
+                   player2Score--;
+               } else {
+                   player1Score = player2Score = 0; 
+               }
+               break;
+            case 4:
+            default:
+               throw new Error('Illegal score');
+        }
+    };
+
+    Match.prototype.player2Scores =
+    function() {
+        switch (player2Score) {
+            case 0:
+            case 1:
+            case 2:                   
+                player2Score++;
                 break;
             
             default:
--- test.js
+++ test.js
@@ -7,8 +7,8 @@ describe('Match', function() {
             player1: 'Nadal', 
             player2: 'Federer'});
 
-        assert.equal(0, match.player1Score());
-        assert.equal(0, match.player2Score());
+        assert.equal('Love', match.player1Score());
+        assert.equal('Love', match.player2Score());
     });
 
     it('should be 15-0 if player 1 scores', function() {
@@ -17,11 +17,42 @@ describe('Match', function() {
             player2: 'Federer'});
 
         match.player1Scores();
-        assert.equal(15, match.player1Score());
-        assert.equal(0, match.player2Score());
+        assert.equal('Fifteen', match.player1Score());
+        assert.equal('Love', match.player2Score());
     });
 
-    it('should score 40-40', function() {});
+    it('should score 40-40', function() {
+        var match = new Match({
+            player1: 'Nadal',
+            player2: 'Federer'});
 
-    it('should consider player 1 advantage', function() {});
+        match.player1Scores(); // 15-0
+        match.player1Scores(); // 30-0
+        match.player1Scores(); // 40-0
+
+        match.player2Scores(); // 40-15
+        match.player2Scores(); // 40-30
+        match.player2Scores(); // 40-40
+
+        assert.equal('Forty', match.player1Score());
+        assert.equal('Forty', match.player2Score());
+    });
+
+    it('should consider player 1 advantage', function() {
+        var match = new Match({
+            player1: 'Nadal',
+            player2: 'Federer'});
+
+        match.player1Scores(); // 15-0
+        match.player1Scores(); // 30-0
+        match.player1Scores(); // 40-0
+
+        match.player2Scores(); // 40-15
+        match.player2Scores(); // 40-30
+        match.player2Scores(); // 40-40
+
+        match.player1Scores(); // Ad p1
+        
+        assert.equal('Advantage', match.player1Score());
+    });
 });
